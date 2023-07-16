650c320e09db34df9e726e2fd08d4083c891a2d5
hellenkuttery <helen@clarusway.com>
2023-07-16 22:05:18 +0300

    add wheather app

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # js-Wheather-App
+
+## Description
+Project aims to create a fething Whether information from an Api with city name and shows it.
+
+## Project Skeleton 
+
+```
+Whether App
+|
+|----readme.md                   
+|----img  
+    |----img       
+|----js
+    |----Aes.js
+    |----Extension.js
+    |----wheather.js
+|----css
+    |----style.css    
+|----index.html 
+```
+## Project Gif
+
+![js Slider](img/wheather_app.gif)
+
+## Project Link
+[Simple JS Slider](https://hellenkuttery.github.io/js-slider/)
---
+++ js/weather.js
@@ -0,0 +1,66 @@
+const form = document.querySelector("form");
+const input = document.querySelector("form input");
+const msgSpan = form.querySelector(".msg");
+const list = document.querySelector(".container .cities");
+
+//
+
+//!Bu bilgiyi normalde .env de tutarız ama şimdilik localStorage da tutalım
+
+localStorage.setItem(
+  "apiKey",
+  EncryptStringAES("be82f27c418b2db61311299d002e474e")
+);
+
+//NOT: onClick 4 yöntemle kullnabiliriz. İnline,addeventlistener,onclick, setAttribute("submit",submitFunction) gibi
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherApi();
+  // refresh olduğunda boşaltmak için kullanıyoruz.
+  e.target.reset();
+});
+
+const getWeatherApi = async () => {
+  const ApiKey = DecryptStringAES(localStorage.getItem("apiKey"));
+  console.log(ApiKey);
+  const cityName = input.value;
+  console.log(cityName);
+  const units = "metric";
+  const lang = "tr";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${ApiKey}&units=${units}&lang=${lang}`;
+
+  try {
+    const response = await fetch(url).then((res) => res.json());
+    console.log(response);
+
+    // Object destructuring yapalım
+
+    const { main, name, sys, weather } = response;
+    // const iconUrl=`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+    // console.log(iconUrl);
+
+    const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+    console.log(iconUrlAWS);
+
+    // Uzun yöntem ile element ekleme
+
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    createdLi.innerHTML = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+          </h2>
+          <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+          <figure>
+                <img class="city-icon" src="${iconUrlAWS}">
+                <figcaption>${weather[0].description}</figcaption>
+          </figure>
+        `
+    list.append(createdLi)
+  } catch (error) {
+    console.log(error);
+    msgSpan.innerText = "City not found";
+  }
+};
