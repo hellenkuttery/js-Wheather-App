8069bfc7906c9f84a1d0a4a0fd79ae276bef34ad
hellenkuttery <helen@clarusway.com>
2023-09-02 07:53:19 +0300

    change colors

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,20 @@ const input = document.querySelector("form input");
 const msgSpan = form.querySelector(".msg");
 const list = document.querySelector(".container .cities");
 
+//
+
+//!Bu bilgiyi normalde .env de tutarız ama şimdilik localStorage da tutalım
+
 localStorage.setItem(
   "apiKey",
   EncryptStringAES("be82f27c418b2db61311299d002e474e")
 );
 
+//NOT: onClick 4 yöntemle kullnabiliriz. İnline,addeventlistener,onclick, setAttribute("submit",submitFunction) gibi
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   getWeatherApi();
+  // refresh olduğunda boşaltmak için kullanıyoruz.
   e.target.reset();
 });
 
@@ -27,29 +33,37 @@ const getWeatherApi = async () => {
   try {
     const response = await fetch(url).then((res) => res.json());
     console.log(response);
+
+    // Object destructuring yapalım
+
     const { main, name, sys, weather } = response;
     // const iconUrl=`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
     // console.log(iconUrl);
-    /* -------------------------------------------------------------------------- */
+
     const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
     console.log(iconUrlAWS);
-
     /* -------------------------------------------------------------------------- */
+    const cityNameSpan=list.querySelectorAll("span")
+    if (cityNameSpan.length >0){
+      const filteredArray=[...cityNameSpan].filter(
 
-    const cityNameSpan = list.querySelectorAll("span");
-    if (cityNameSpan.length > 0) {
-      const filteredArray = [...cityNameSpan].filter(
-        (span) => span.innerHTML == name
-      );
-      if (filteredArray.length > 0) {
-        msgSpan.innerText = ` You have already listed this city`;
-        setTimeout(() => {
-          msgSpan.innerText = ``;
-        }, 3000);
-        return;
-      }
+        //     (span)=>span.innerHTML==cityName); bu kısımdaki cityName kısmını eğer bir şehrin adını farklı şekillerde Konya KONYA gibi yazarsak ikisini de gösteriyor.Bu nedenle apiden geldiği haliyle name ile karşılaştırıyoruz
+        (span)=>span.innerHTML==name);
+    if (filteredArray.length>0){
+
+      // mesaj biraz beklesin gitsin diye bu kodu
+      msgSpan.innerText=` You have already listed this city`
+      // setTimeout ekleyerek geri boş gösterelim
+
+      setTimeout(()=>{
+        msgSpan.innerText=``
+      },3000)
+      return
+        }
+      
     }
     /* -------------------------------------------------------------------------- */
+    // Uzun yöntem ile element ekleme
     const createdLi = document.createElement("li");
     createdLi.classList.add("city");
     createdLi.innerHTML = `
@@ -64,12 +78,11 @@ const getWeatherApi = async () => {
           </figure>
         `;
     list.prepend(createdLi);
-    /* -------------------------------------------------------------------------- */
   } catch (error) {
     console.log(error);
     msgSpan.innerText = "City not found";
-    setTimeout(() => {
-      msgSpan.innerText = ``;
-    }, 3000);
+    setTimeout(()=>{
+      msgSpan.innerText=``
+    },3000)
   }
 };
